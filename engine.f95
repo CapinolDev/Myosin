b7728fc879c6118a8522591736940a7839bc8fe8
ERDINC DASKIN <eruanio33@gmail.com>
2025-11-29 23:50:42 +0100

    Added some logic

--- engine.f95
+++ engine.f95
@@ -1,27 +1,81 @@
+
 program engine
+
     implicit none
     
     character, allocatable :: a1(:),a2(:),a3(:)
     character, allocatable :: b1(:),b2(:),b3(:)
     character, allocatable :: c1(:),c2(:),c3(:)
-    character(len=2) :: selLoc
+    character(len=2) :: selLoc,currPlayer
+    character :: selPiece
+    logical :: selWrongLoc, game, allGood
+    integer :: i
+
+    type :: Inventory
+        integer :: bAmnt = 10
+        integer :: yAmnt = 10
+        integer :: gAmnt = 10
+        integer :: pAmnt = 10
+        integer :: rAmnt = 10
+    end type Inventory
+
+    type :: Player
+        integer :: points = 0
+        type(Inventory) :: inventory
+    end type Player
+
+    type(Inventory) :: p1Inv,p2Inv
+
+    type(Player),target :: p1,p2
+    type(Player), pointer :: currP
+
+    p1%Inventory = p1Inv
+    p2%Inventory = p2Inv
+
+    game = .true.
 
     allocate(a1(1)); allocate(a2(1)); allocate(a3(1));
     allocate(b1(1)); allocate(b2(1)); allocate(b3(1)); 
     allocate(c1(1)); allocate(c2(1)); allocate(c3(1));
 
-
-
-
-
-    do while (.true.)
-
-    call printBoard
-    write(*,'(A)', advance='no')"Enter location: "
-    read(*,*) selLoc
-
-    end do
     
+
+
+
+    selWrongLoc = .false.
+
+    currPlayer = "p2"
+    do while (game)
+        if (currPlayer=="p2") then 
+            currPlayer = "p1"
+            currP => p1
+        else
+            currPlayer = "p2"
+            currP => p2
+        end if
+
+        call system("clear")
+        
+        write(*,'(A)')currPlayer
+        call printInv
+        if (selWrongLoc .eqv. .true.)  then
+            write(*,'(A)') "Error: invalid location"
+            selWrongLoc = .false.
+        end if
+        call printBoard
+        write(*,'(A)', advance='no')"Enter location: "
+        read(*,*) selLoc
+        write(*,'(A)', advance='no')"Enter piece: "
+        read(*,*) selPiece
+        allGood = HasPieceUpdate(currP,selPiece)
+        
+        if (allGood) then 
+            call resolveInput(selLoc,selPiece)
+        end if
+
+        
+    end do
+
     contains
 
     pure function pushStone(location, stone) result(res)
@@ -37,7 +91,76 @@ program engine
 
     end function
 
-     subroutine printBoard()
+    function HasPieceUpdate(playr, piece) result(res)
+        type(Player) :: playr
+        character :: piece
+        logical :: res
+
+
+        select case(piece)
+            case ("b")
+                if (playr%inventory%bAmnt > 0) then 
+                    playr%inventory%bAmnt = playr%inventory%bAmnt - 1
+                    res = .true.
+                    return 
+                end if
+            case("y") 
+                if (playr%inventory%yAmnt > 0) then 
+                    playr%inventory%yAmnt = playr%inventory%yAmnt - 1
+                    res = .true.
+                    return 
+                end if     
+            case("g") 
+                if (playr%inventory%gAmnt > 0) then 
+                    playr%inventory%gAmnt = playr%inventory%gAmnt - 1
+                    res = .true.
+                    return 
+                end if
+            case("p") 
+                if (playr%inventory%pAmnt > 0) then 
+                    playr%inventory%pAmnt = playr%inventory%pAmnt - 1
+                    res = .true.
+                    return 
+                end if   
+            case("r") 
+                if (playr%inventory%rAmnt > 0) then 
+                    playr%inventory%rAmnt = playr%inventory%rAmnt - 1
+                    res = .true.
+                    return 
+                end if             
+        end select
+    end function
+
+    subroutine resolveInput(location, stone)
+        character(len=2) :: location 
+        character :: stone
+
+        select case(location)
+            case("a1")
+                a1 = pushStone(a1,stone)
+            case("a2")
+                a2 = pushStone(a2,stone)    
+            case("a3")
+                a3 = pushStone(a3,stone)    
+            case("b1")
+                b1 = pushStone(b1,stone)
+            case("b2")
+                b2 = pushStone(b2,stone)    
+            case("b3")
+                b3 = pushStone(b3,stone)
+            case("c1")
+                c1 = pushStone(c1,stone)
+            case("c2")
+                c2 = pushStone(c2,stone)    
+            case("c3")
+                c3 = pushStone(c3,stone)
+            case default
+                selWrongLoc = .true.
+        end select
+
+    end subroutine
+
+    subroutine printBoard()
         write(*,'(A)', advance='no') a1(size(a1))
         write(*,'(A)', advance='no') "|"
         write(*,'(A)', advance='no') a2(size(a2))
@@ -56,6 +179,20 @@ program engine
         write(*,'(A)', advance='no') "|" 
         write(*,'(A)', advance='no') c3(size(c3))
         write(*,'(A)')
+    end subroutine
+
+    subroutine printInv()
+        write(*,'(A)',advance='no') "b: "
+        write(*,'(I0)',advance='no') currP%Inventory%bAmnt
+        write(*,'(A)',advance='no') " y: "
+        write(*,'(I0)',advance='no') currP%Inventory%yAmnt
+        write(*,'(A)',advance='no') " g: "
+        write(*,'(I0)',advance='no') currP%Inventory%gAmnt
+        write(*,'(A)',advance='no') " p: "
+        write(*,'(I0)',advance='no') currP%Inventory%pAmnt
+        write(*,'(A)',advance='no') " r: "
+        write(*,'(I0)') currP%Inventory%rAmnt
+
 
 
 
